27593919273292ce4d8c17e6783a9f605202d2e5
Sushi-Samurai <122423970+Sushi-Samurai@users.noreply.github.com>
2024-06-01 10:26:48 -0700

    Built basic dictionary search function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h3 class="logo d-none d-md-block">Sam Turner</h3>
-        <h3 class="logo d-block d-md-none">ST</h3>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <h3 className="logo d-none d-md-block">Sam Turner</h3>
+          <h3 className="logo d-block d-md-none">ST</h3>
+        </header>
+
+        <main>
+          <Dictionary />
+        </main>
+
+        <footer className="App-footer">
+          <small>
+            ✌ This project was coded by Sam Turner-Lara and it is{" "}
+            <a href="https://github.com/Sushi-Samurai/react-dictionary0.1">
+              open-sourced
+            </a>
+            .
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
