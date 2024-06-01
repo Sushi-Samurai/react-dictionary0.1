199ae02cf6b4269ad58335317c02732c0975dc0c
Sushi-Samurai <122423970+Sushi-Samurai@users.noreply.github.com>
2024-06-01 09:20:18 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3 class="logo d-none d-md-block">Sam Turner</h3>
+        <h3 class="logo d-block d-md-none">ST</h3>
+      </header>
+    </div>
+  );
+}
