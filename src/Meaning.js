d7b4817db5b21f90fe4a296770dd9a242d2467b9
Sushi-Samurai <122423970+Sushi-Samurai@users.noreply.github.com>
2024-06-05 20:03:26 -0700

    Rendered synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,11 +1,21 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
+      <br />
+      <strong>Definition:</strong>
       <p>{props.meaning.definition}</p>
-      <p>{props.meaning.example}</p>
+      <br />
+      <strong>Example:</strong>
+      <p>
+        <i>{props.meaning.example}</i>
+      </p>
+      <br />
+
+      <Synonyms synonyms={props.meaning.synonyms} />
     </div>
   );
 }
